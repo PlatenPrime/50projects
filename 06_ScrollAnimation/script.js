90f8225bfbfad54a9a46aee7e8545094a06b9b78
Aleksandr Marchenko <proshta@gmail.com>
2024-11-21 16:52:47 +0200

    21.11.2024

--- 06_ScrollAnimation/script.js
+++ 06_ScrollAnimation/script.js
@@ -5,7 +5,7 @@ window.addEventListener("scroll", checkBoxes);
 checkBoxes();
 
 function checkBoxes() {
-  const triggerBottom = (window.innerHeight / 3) * 2;
+  const triggerBottom = (window.innerHeight / 2) ;
 
   boxes.forEach((box) => {
     const boxTop = box.getBoundingClientRect().top;
